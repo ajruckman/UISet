7bfb35d6cb9667257369aab2f2041e33302c526a
A.J. Ruckman <aj@ruckman.dev>
2020-03-02 19:25:43 -0500

    compiled themes

---
+++ ShapeSet/_build.ps1
@@ -0,0 +1,13 @@
+Remove-Item -Force -Recurse .\wwwroot\css\
+New-Item -Type Directory .\wwwroot\css\
+
+foreach ($theme in $(Get-ChildItem "..\ColorSet\Themes\" -Directory))
+{
+    Write-Output "Building stylesheet: $($theme.FullName)"
+    & sassc -m "Style.$($theme.Name).scss" ".\wwwroot\css\Style.$($theme.Name).css"
+
+    if (-not $?)
+    {
+        exit 2
+    }
+}
