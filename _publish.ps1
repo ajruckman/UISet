b46d737f32c2ca9adf18635aeb47136432097502
AJ Ruckman <aj.ruckman@mckee.com>
2020-03-06 08:11:30 -0500

    - Update _publish.ps1 to build symbol packages

--- _publish.ps1
+++ _publish.ps1
@@ -1,7 +1,7 @@
 function Clean-DotNETProject
 {
     Get-ChildItem .\ -include bin, obj -Recurse | foreach ($_) {
-        Write-Output "Remove: $($_.FullName)"
+        Write-Output "Remove: $( $_.FullName )"
         Remove-Item $_.FullName -Force -Recurse
     }
 }
@@ -17,7 +17,7 @@ cd .\ShapeSet\
 .\_build.ps1
 cd ..
 
-dotnet pack -c Debug
+dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 if (!(Test-Path .\_published\))
 {
@@ -26,12 +26,13 @@ if (!(Test-Path .\_published\))
 rm -Force .\_published\*
 
 Get-ChildItem -Directory | foreach {
-	if (Test-Path "$($_.FullName)\bin") {
-		Get-ChildItem "$($_.FullName)\bin\" -Depth 1 -Filter *.nupkg | foreach {
-			Write-Output "Copying package: $($_.Name)"
-			Copy-Item $_.FullName .\_published\
-		}
-	}
+    if (Test-Path "$( $_.FullName )\bin")
+    {
+        Get-ChildItem "$($_.FullName)\bin\" -Depth 1  | ? { ($_.FullName -like "*.nupkg") -or ($_.FullName -like "*.snupkg") } | foreach {
+            Write-Output "Copying package: $( $_.Name )"
+            Copy-Item $_.FullName .\_published\
+        }
+    }
 }
 
 Remove-Item -ErrorAction Ignore -Force -Recurse $HOME\.nuget\packages\uiset.colorset\
